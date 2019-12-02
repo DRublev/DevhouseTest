05052b40601d21425a8461b3f4b98db8da023c9a
DRublev <aringai09>
2019-12-02 16:35:09 +0300

    Update and dele routes

--- backend/routes/api/shops.js
+++ backend/routes/api/shops.js
@@ -22,3 +22,22 @@ router.post('/create', auth.required, (req, res, next) => {
 
     finalShop.save().then(() => res.redirect('/'));
 });
+
+
+router.post('/update', auth.required, (req, res, next) => {
+    const {
+        body: { shop, editedShop }
+    } = req;
+
+    //TODO: need refactoring
+    Shops.update({ _id: shop.id }, editedShop, (err) => console.debug(err));
+});
+
+router.post('/delete', auth.required, (req, res, next) => {
+    const {
+        body: { shop }
+    } = req;
+
+    //TODO: need refactoring
+    Shops.deleteOne({ _id: shop.id }, (err) => console.debug(err));
+});
