ddbe9f1eadb1961143499740f7166d7258217f59
DRublev <aringai09>
2019-12-04 10:00:25 +0300

    Auth functional

--- devhouse/src/Auth/Login.js
+++ devhouse/src/Auth/Login.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import Parent from '../Parent.js';
-import Users from '../helpers/Api/Users.js';
+import Users from '../helpers/Api/Users';
 
 
 let helpers = require('../helpers/functions.js');
